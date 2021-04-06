72a3245ea96b35950fa3a18ac0b32d68f81b966c
Yoichi1411 <71240361+Yoichi1411@users.noreply.github.com>
2021-04-06 19:21:08 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(function(){
+    //動画切り替え
+    $('#movie1').click(function(){
+        $('.movie-wrapper2,.movie-wrapper3,.movie-wrapper4').fadeOut(0);
+        $('.movie-wrapper1').fadeIn();
+    });
+
+    $('#movie2').click(function(){
+        $('.movie-wrapper1,.movie-wrapper3,.movie-wrapper4').fadeOut(0);
+        $('.movie-wrapper2').fadeIn();
+    });
+
+    $('#movie3').click(function(){
+        $('.movie-wrapper1,.movie-wrapper2,.movie-wrapper4').fadeOut(0);
+        $('.movie-wrapper3').fadeIn();
+    });
+
+    $('#movie4').click(function(){
+        $('.movie-wrapper1,.movie-wrapper2,.movie-wrapper3').fadeOut(0);
+        $('.movie-wrapper4').fadeIn();
+    });
+});
+
+
+//ページ読み込み後の実行プログラム
+window.onload = function() {
+    //
+    
+};
