90810c8892e2c87f6f00de63b0ca38c08a653f71
Yoichi1411 <71240361+Yoichi1411@users.noreply.github.com>
2021-04-07 13:35:44 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 $(function(){
     //動画切り替え
-    $('#movie1').click(function(){
+    $('#movie1,.prev-button2').click(function(){
         $('.movie-wrapper2,.movie-wrapper3,.movie-wrapper4').fadeOut(0);
         $('.movie-wrapper1').fadeIn();
     });
 
-    $('#movie2').click(function(){
+    $('#movie2,.next-button1,.prev-button3').click(function(){
         $('.movie-wrapper1,.movie-wrapper3,.movie-wrapper4').fadeOut(0);
         $('.movie-wrapper2').fadeIn();
     });
 
-    $('#movie3').click(function(){
+    $('#movie3,.next-button2,.prev-button4').click(function(){
         $('.movie-wrapper1,.movie-wrapper2,.movie-wrapper4').fadeOut(0);
         $('.movie-wrapper3').fadeIn();
     });
 
-    $('#movie4').click(function(){
+    $('#movie4,.next-button3').click(function(){
         $('.movie-wrapper1,.movie-wrapper2,.movie-wrapper3').fadeOut(0);
         $('.movie-wrapper4').fadeIn();
     });
